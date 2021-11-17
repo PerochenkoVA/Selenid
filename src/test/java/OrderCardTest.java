0cad8be140e912939527ae839a1ef227db338ff1
Uzbek150885 <Uzbek150885@gmail.com>
2021-11-18 00:00:14 +0300

    name+tel+button2

--- src/test/java/OrderCardTest.java
+++ src/test/java/OrderCardTest.java
@@ -1,15 +1,9 @@
-import com.codeborne.selenide.Selenide;
-import com.codeborne.selenide.SelenideElement;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 
 import static com.codeborne.selenide.Condition.exactText;
-import static com.codeborne.selenide.Selenide.*;
-
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 
 public class OrderCardTest {
 
@@ -17,14 +11,15 @@ public class OrderCardTest {
     @Test
     void testApplication() {
         open("http://localhost:9999");
-      //  SelenideElement form = $("[data-test-id]");
+        //  SelenideElement form = $("[data-test-id]");
 
         $("[data-test-id = 'name'] input").setValue("Владимир");
         $("[data-test-id  = 'phone'] input").setValue("+79268889977");
-        $("[name = 'agreement'] input").click();
-        $("[class='button__text']").click();
+        $("span.checkbox__box").click();
+        $("span.button__text").click();
 
-        $("").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+        $(withText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.")).shouldBe();
 
     }
 }
+
